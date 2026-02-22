83537a732769b6a5d165b4de036be146e14a23dd
cjc <curtisjcampbell@mail.com>
2026-02-22 19:51:39 +0000

    feat(map): use 3:1 x sampling ratio

--- examples/map-mode.ts
+++ examples/map-mode.ts
@@ -136,7 +136,7 @@ const renderViewport = (
   matrix.clear().fgColor(config.landColorHex);
 
   const bandRows = config.worldYEnd - config.worldYStart;
-  const xScale = 1;
+  const xScale = 3;
 
   for (let y = 0; y < viewHeight; y += 1) {
     const worldY =
